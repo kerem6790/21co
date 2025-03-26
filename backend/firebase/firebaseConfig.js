695e81b776c5c5e2d5652bd981c597a35adde6dc
KEREM GEGEK <kgege@MacBook-Air-5.local>
2025-03-26 21:12:19 +0300

    Dosya düzenlemsi

---
+++ backend/firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "XXX",
+  authDomain: "XXX.firebaseapp.com",
+  projectId: "XXX",
+  storageBucket: "XXX.appspot.com",
+  messagingSenderId: "XXX",
+  appId: "XXX"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
